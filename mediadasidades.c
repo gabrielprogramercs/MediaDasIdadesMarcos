4e13496918c5a99b8225d0268f6603c57bc2458c
Gabriel Lopes <gabriel-fernando2008@hotmail.com>
2026-05-14 17:06:51 -0300

    Create mediadasidades.c

---
+++ mediadasidades.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int idade;
+    int soma = 0;
+    int quantidade = 0;
+    float media;
+
+    printf("Digite as idades (0 para encerrar):\n");
+    scanf("%d", &idade);
+
+    while (idade != 0) {
+        soma = soma + idade;
+        quantidade = quantidade + 1;
+
+        
+        scanf("%d", &idade);
+    }
+
+    if (quantidade > 0) {
+        media = (float)soma / quantidade;
+        printf("Media das idades: %.2f\n", media);
+    } else {
+        printf("Nenhuma idade valida foi digitada.\n");
+    }
+
+    return 0;
+}
